904654cf39097f8446c04fc278b4725806bcafa4
ajorza <ajorza@gmail.com>
2025-11-18 16:31:37 -0500

    Update zeros.py

--- zeros.py
+++ zeros.py
@@ -1,1 +1,12 @@
+########################################################
+# Main Repository of Functions for Zeros of MF Project #
+########################################################
+
+# Fall 2025
+
+# Functions to get Modular Forms
+
+# Functions to get Faber Polynomials
+
+# Functions to find zeros of MF
 
