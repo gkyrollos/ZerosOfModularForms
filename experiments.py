65e406c899c1557192a242df5263c0fec8946e8e
ajorza <ajorza@gmail.com>
2025-12-05 14:52:08 -0500

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -1,3 +1,17 @@
 # Experiments
 
 from zeros import *
+
+##########################
+# family #1 
+# sum a_i E_{k+i} E_{l-i}
+##########################
+
+##########################
+# family #1 
+# sum a_ij E_{k+u} E_{l+v} E_{m+w}
+##########################
+
+# plots for animation of the 
+# variation of zeros as the form
+# varies in a family
